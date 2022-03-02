4cda830cc30432294d8f77d3b72da528e34ea6ee
imjee1 <wldud3234@gmail.com>
2022-03-02 23:15:00 +0900

    재귀

---
+++ src/boj/recursive/boj10870/Main.java
@@ -0,0 +1,18 @@
+package boj.boj10870;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //0 1 1 2 3 5 8
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+
+//        public static int fibonacci(int num) {
+
+//        }
+    }
+}
---
+++ src/boj/recursive/boj10872/Main.java
@@ -0,0 +1,2 @@
+package boj.recursive.boj10872;public class Main {
+}
